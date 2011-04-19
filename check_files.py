2edd586924534b30d342805d84569086cd2b331c
Swizec Teller <swizec@swizec.com>
2011-04-19 19:27:07 +0200

    outline of some things that need to happen

---
+++ check_files.py
@@ -0,0 +1,17 @@
+
+# checking for changes in a file
+# store last known metadata
+# traverse dir
+# - if file changed after last known change
+#   - compare hash to last hash
+#   - if hash is different then make a commit and push
+#
+# check if it's possible to be notified of file changes
+#
+# what happens when there are conflicts?
+# what about branches?
+# how to present all of this to the user?
+
+# how to install everything? Mkae sure it works?
+# needs git
+# git is weird on windows ...
