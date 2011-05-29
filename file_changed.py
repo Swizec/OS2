a7daf17cdbd7be3bbae98a170988ecae2de109eb
Dudu <Rok@.(none)>
2011-05-30 01:38:02 +0200

    eto to je to use kar rabs je v komentarjih od file_changed.py - uzivaj :P

--- file_changed.py
+++ file_changed.py
@@ -1,52 +1,23 @@
 
-import os
-
-import win32file
-import win32con
-
-#path_to_watch = "." look at the current directory
-def run_listener(path_to_watch,dataz):
-    # Set up the bits we'll need for output
-    ACTIONS = {
-      1 : "Created",
-      2 : "Deleted",
-      3 : "Updated",
-      4 : "Renamed from something",
-      5 : "Renamed to something"
-    }
-    FILE_LIST_DIRECTORY = 0x0001
-    hDir = win32file.CreateFile (
-      path_to_watch,
-      FILE_LIST_DIRECTORY,
-      win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE,
-      None,
-      win32con.OPEN_EXISTING,
-      win32con.FILE_FLAG_BACKUP_SEMANTICS,
-      None
-    )
-
-    while 1:
-      # Wait for a change to occur
-      results = win32file.ReadDirectoryChangesW(hDir, 1024, True, win32con.FILE_NOTIFY_CHANGE_LAST_WRITE, None, None)
-      #print results
-      # For each change, check to see if it's updating the file we're interested in
-      for action, file in results:
-        full_filename = os.path.join (path_to_watch, file)
+# this is called after a thread detects a file change
+def file_changed(full_filename, action, dataz):
         #what to do when a file is changed
-        print "File changed: %s"%full_filename
-        
+        #print action
+        #print "File changed: %s"%full_filename
+        pass
+        #
         # @ SWIZC
         #to se laufa ko se spremeni fajl, mas pa dictionary "dataz" z naslednjo vsebino:
         #{'Username': u'user', 'Password': u'pass', 'Location': u'D:\\Games', 'Repository': u'rep_name'}
-        
-        
-if __name__ == '__main__':
-    run_listener(".\\test")
-        
-        
-        
-        
-        
-        
-        
-        
+        #
+        #action spremenljivka (tipa int) ti pove kaj je bil action izveden nad fajlom. moznosti so sledece:
+        #  1 : "Created"
+        #  2 : "Deleted"
+        #  3 : "Updated"
+        #  4 : "Renamed from something"
+        #  5 : "Renamed to something"
+        #
+        # pazi: 
+        # - ob spremembi imena fajla se tole klice trikrat; enkrat 4 za star fajl, pol pa 5 za tanovga, pol pa se 3 za tanovga >_< deal with it, men se neda :P
+        #
+ 
--- threads.py
+++ threads.py
@@ -1,7 +1,9 @@
 
-from threading import Thread
+from threading import *
 import file_changed as fc
-
+import os
+import win32file
+import win32con
 
 #---------------- Thread ----------------------------------------
 
@@ -11,16 +13,73 @@ class FileListener(Thread):
         Thread.__init__(self)
         self.directory = directory
         self.dataz = dataz
-    
+        # sets repeat flag to true
+        self.r = 1
+        # Set up the bits we'll need for output
+        ACTIONS = {
+          1 : "Created",
+          2 : "Deleted",
+          3 : "Updated",
+          4 : "Renamed from something",
+          5 : "Renamed to something"
+        }
+        FILE_LIST_DIRECTORY = 0x0001
+        self.hDir = win32file.CreateFile (
+          self.directory,
+          FILE_LIST_DIRECTORY,
+          win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE,
+          None,
+          win32con.OPEN_EXISTING,
+          win32con.FILE_FLAG_BACKUP_SEMANTICS,
+          None
+        )
+        
     def run(self):
-        fc.run_listener(self.directory,self.dataz)
+        taction = 0
+        tfile = ''
+        while 1:
+          # this ends thread when r is set to 0... i think
+          if self.r == 0:
+            break
+          # Wait for a change to occur
+          results = win32file.ReadDirectoryChangesW (
+            self.hDir,
+            1024,
+            True,
+            win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+             win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+             win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+             win32con.FILE_NOTIFY_CHANGE_SIZE |
+             win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+             win32con.FILE_NOTIFY_CHANGE_SECURITY,
+            None,
+            None
+          )
 
+          # this ends thread when r is set to 0... i think
+          if self.r == 0:
+            break
+          # For each change, check to see if it's updating the file we're interested in
+          for action, file in results:
+            if (action, file) != (taction, tfile):
+                full_filename = os.path.join (self.directory, file)
+                fc.file_changed(full_filename, action, self.dataz)
+                taction = action
+                tfile = file
+            else:
+                taction = 0
+                tfile = ''
+        #print "thread stopped!"
+    
+    def stop(self):
+        self.r = 0
         
 #---------------- Methods ---------------------------------------
 
 class ThreadMaster():
-    #defining empty list of threads
+    # starting threads
     def start(self, dataz):
+        #defining empty list of threads
         self.threads = []
         #running threads for each repository
         for key in dataz.keys():
@@ -29,21 +88,22 @@ class ThreadMaster():
             thread=FileListener(dataz[key]['Location'],dataz[key])
             thread.start()
             self.threads.append(thread)
-            print "Listening to "+dataz[key]['Repository']+" located in "+dataz[key]['Location']
+            #print "Listening to "+dataz[key]['Repository']+" located in "+dataz[key]['Location']
 
             
     #stoping threads
     def stop(self):
         for thread in self.threads:
             if thread.isAlive():
-                try:
-                    #magical unicorns murder the thread with sugary goodness
+                #sets the loop flag in the thread to false so the thread will stop eventually.
+                thread.stop()
+                try:#apparently this is how youre suppolsed to kill a thread in python but it doesnt seem to work..
                     thread._Thread__stop()
-                    #print "stopping threads"
                 except:
                     print(str(thread.getName()) + ' could not be terminated')
-
-
+                
+                
+    #restarts the threads.
     def restart(self, dataz):
         stop()
         start(dataz)
